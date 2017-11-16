f69e2092482dd4c9f5f10c75aeb8ea2088ebf0fb
discreteb <discreteb@gmail.com>
2017-11-17 02:23:06 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+app.get('/', function (req, res) { res.send('Welcome!');});
+app.get('/tweets/:lat/:long', require('./controllers/twitterController'));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.listen(app.get('port'), app.get('ip'), function () {
+
+    console.log( "Globe api on " + app.get('ip') + ", server_port " + app.get('port')  );
+
+});
---
+++ controllers/twitterController.js
@@ -0,0 +1,110 @@
+let commonWords = require('../common_words.json');
+
+let TwitterController = function(req, res){
+
+	let Twitter = require('twitter');
+	let TwitterConfig = {
+			consumer_key: process.env.consumer_key,
+			consumer_secret: process.env.consumer_secret,
+			access_token_key: process.env.access_token_key,
+			access_token_secret: process.env.access_token_secret
+		};
+
+	if (!process.env.consumer_key || 
+		!process.env.consumer_secret || 
+		!process.env.access_token_key || 
+		!process.env.access_token_secret ) {
+		TwitterConfig = require(process.cwd()+'/config.json').twitter
+	}
+
+	let twitterClient = new Twitter(TwitterConfig);
+
+	let getAvailableWoeid = function(lat, long, callback){
+		let params = {
+			lat: lat,
+			long: long
+		};
+
+		let requestUrl = '/trends/closest.json';
+
+		twitterClient.get(requestUrl, params, function(error, location, response) {
+			let contents = [];
+			if (!error) {
+				try{
+					callback(null, location[0]);
+				}catch(e){
+					callback(e, null);
+				}
+			}else{
+				callback(JSON.stringify(error), null);
+			}
+		});
+	}
+
+	let getKeywords = function(location, callback){
+
+		let params = {
+			id:location.woeid
+		};
+		
+		let requestUrl = '/trends/place.json';
+
+		twitterClient.get(requestUrl, params, function(error, tweets, response) {
+			let contents = [];
+			if (!error) {
+				try{
+					callback(null, tweets);
+				}catch(e){
+					callback(e, null);
+				}
+			}else{
+				callback(JSON.stringify(error), null);
+			}
+		});
+	}
+
+	let handleKeywords = function(keywords, location){
+		let ret = [];
+		let totalPoint = 0;
+		for (var i = 0; i < keywords.length; i++) {
+			var word = keywords[i].name.replace('#','');
+			var point = keywords[i].tweet_volume || 0;
+			if (commonWords.indexOf(word) === -1) {
+				totalPoint += keywords[i].tweet_volume;
+				ret.push({word:word, point:point});	
+			}
+		}
+		
+		ret.sort(function(a, b){
+			return b.point-a.point
+		});
+
+		return {
+			topics:ret,
+			location:location,
+			totalPoint:totalPoint
+		};
+	}
+
+	let initData = function(){
+
+		getAvailableWoeid(req.params.lat, req.params.long, function(err, location){
+			if (err) {
+				res.send(err);	
+			}else{
+				getKeywords(location, function(err, result){
+					if (err) {
+						res.send(err);	
+					}else{
+						let ret = handleKeywords(result[0].trends, result[0].locations[0].name);
+						res.send(ret);
+					}
+				});
+			}
+		});
+	}
+
+	initData();
+};
+
+module.exports = TwitterController;
